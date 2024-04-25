4af825bd3697b34c938958e67f9b9881b51b2b28
Akshatha S Charantimath <akshathacharantimath666@gmail.com>
2024-04-25 15:07:29 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+const Navigation = () => {
+   return (
+   <nav className="container">
+      <div className="logo">
+
+         <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+
+             <ul>
+               <li href="#"><button>Menu</button></li>
+             <li href="#"><button>Location</button></li>
+             <li href="#"><button>About</button></li>
+             <li href="#"><button>Contact</button> </li> 
+             </ul>
+
+
+
+              <button>login</button>
+    </nav>
+   );
+
+
+  
+   
+
+
+}
+
+export default Navigation;
